f1f7140117fbe35a7216bc067949b776c6276c6b
zhoumiaomiao0606 <741715025@qq.com>
2020-05-24 20:02:19 +0800

    Signed-off-by: zhoumiaomiao0606 <741715025@qq.com>

---
+++ src/main/java/com/cktest/pojo/User.java
@@ -0,0 +1,36 @@
+package com.cktest.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 周周
+ * @since 2020-05-24
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="User对象", description="")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String emial;
+
+    private String password;
+
+
+}
---
+++ src/main/java/com/cktest/service/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.cktest.service.impl;
+
+import com.cktest.pojo.User;
+import com.cktest.mapper.UserMapper;
+import com.cktest.service.UserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author 周周
+ * @since 2020-05-24
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+}
